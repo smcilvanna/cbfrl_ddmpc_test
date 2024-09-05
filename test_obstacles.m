8b4222579fafd3381b7ef812912575a5c5ad2ccf
smcilvanna <smcilvanna01@qub.ac.uk>
2024-09-05 16:19:25 +0100

    add path for other machine, define parameter set and pass to sim

--- test_obstacles.m
+++ test_obstacles.m
@@ -1,4 +1,5 @@
 addpath("~/Documents/MATLAB/com/casadi/");
+% addpath("~/matlab/com/casadi");
 %% Test obstacle set
 % parameter sweep set in test_params
 % obs_test_set = [0.1 0.2 0.3 0.4 0.5 1.1 1.2 1.3 1.4 1.5 ]';
@@ -6,11 +7,11 @@ addpath("~/Documents/MATLAB/com/casadi/");
 %obs_test_set = 0.6;
 
 obs_test_set = [0.1:0.1:2.0]';
-
+test_param_set = [0.001 ; 0.05 ; [0.01 :0.005: 1.00]' ];
 for i = 1:size(obs_test_set,1)
     obs_radius = obs_test_set(i);
 
-    test_params(obs_radius);
+    test_params(obs_radius, test_param_set);
 
 end
 
